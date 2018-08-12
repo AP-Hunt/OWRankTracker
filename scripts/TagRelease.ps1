57b78d20491c95323af427eb3f6b4347b5dd6343
Andy Hunt <andy.p,hunt91@gmail.com>
2018-08-12 19:49:15 +0100

    Adds script for creating a release

---
+++ scripts/TagRelease.ps1
@@ -0,0 +1,24 @@
+﻿Write-Host "Last 5 tags";
+git tag -l | Select -Last 5;
+
+$tag = Read-Host -Prompt "New version tag?";
+
+$releaseNotesDirPath = [System.IO.Path]::Combine((pwd), "ReleaseNotes");
+if(![System.IO.Directory]::Exists($releaseNotesDirPath))
+{
+    New-Item -ItemType Directory -Path $releaseNotesDirPath| Out-Null;
+}
+
+$tagNotesPath = ([System.IO.Path]::Combine($releaseNotesDirPath, "${tag}.txt"));
+Write-Host $tagNotesPath;
+
+if(![System.IO.File]::Exists($tagNotesPath))
+{
+    New-Item -ItemType File -Path $tagNotesPath | Out-Null;
+}
+
+Write-Host "Launching editor for patch notes";
+notepad.exe $tagNotesPath | Out-Null;
+
+Write-Host "Tagging release";
+git tag -a $tag -m "${tag}";
